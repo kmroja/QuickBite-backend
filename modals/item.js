269a20c286e19a3db2b5c2649c0e3bcbba4c86fa
kmroja <rojakmr25@gmail.com>
2026-01-27 19:17:46 +0530

    update

--- modals/item.js
+++ modals/item.js
@@ -11,7 +11,7 @@ const reviewSchema = new mongoose.Schema(
 
 const itemSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
     description: { type: String, required: true },
     category: { type: String, required: true },
     price: { type: Number, required: true },
